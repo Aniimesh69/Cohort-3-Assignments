ff7108cde7af74c9eddc5071cd86814675d91f2a
aniimesh69 <animeshpr152002@gmail.com>
2024-09-24 16:24:00 +0530

    Easy Assignment Section Complete

--- week-2/week-2-js/easy/expenditure-analysis.js
+++ week-2/week-2-js/easy/expenditure-analysis.js
@@ -16,7 +16,22 @@
 function calculateTotalSpentByCategory(transactions) {
   const categoryTotalMap = {};
   
-  return [];
+  transactions.forEach((transaction) => {
+    const { category, price } = transaction;
+
+    if(category in categoryTotalMap){
+      categoryTotalMap[category] += price;
+    }else{
+      categoryTotalMap[category] = price;
+    }
+  });
+
+  const result = [];
+  for(const category in categoryTotalMap){
+    result.push({ category, totalSpent: categoryTotalMap[category] });
+  }
+
+  return result;
 }
 
 module.exports = calculateTotalSpentByCategory;
